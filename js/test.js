8e85c333ea1dbff532e86c376a60cd38b4af6d47
Moundir Jabir <elmondhirjabir@gmail.com>
2022-03-23 14:42:40 +0100

    moundir

--- js/test.js
+++ js/test.js
@@ -4,7 +4,16 @@ let questions = [ // type multichoix ou text
         question: "Pensez-vous avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?", type: "multichoix", choix: ["oui","non","ne sait pas"], complexe: true, sousQuestion: {question: "quelle est votre température ?", type: "text", min: 34, max: 42, unite: "°C"}
     },
     {
-        question: "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", 
+        question: "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", type: "multichoix", choix: ["oui","non"], complexe: false
+    },
+    {
+        question: "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", type: "multichoix", choix: ["oui","non"], complexe: false
+    },
+    {
+        question: "Avez-vous un mal de gorge apparu ces derniers jours ?", type: "multichoix", choix: ["oui","non"], complexe: false
+    },
+    {
+        question: "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", type: "multichoix", choix: ["oui","non"], complexe: false
     }
     //meryeme
 
