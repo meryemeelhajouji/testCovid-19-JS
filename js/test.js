092e5a5c8b226a234e13841dd96f315bb223dcd9
Moundir Jabir <elmondhirjabir@gmail.com>
2022-03-23 14:11:35 +0100

    create tasks collaborator

---
+++ js/test.js
@@ -0,0 +1,14 @@
+let questions = [ // type multichoix ou text
+    // moundir
+    {
+        question: "Pensez-vous avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?", type: "multichoix", choix: ["oui","non","ne sait pas"], complexe: true, sousQuestion: {question: "quelle est votre température ?", type: "text", min: 34, max: 42, unite: "°C"}
+    },
+    {
+        question: "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", 
+    }
+    //meryeme
+
+    //mourad
+
+    //said
+]
