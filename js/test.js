54bd846925261f5fc178bff01d7f31434785cfa8
zerzkhane <zezkhanemourad@gmail.com>
2022-03-23 14:46:54 +0100

    update

--- js/test.js
+++ js/test.js
@@ -5,10 +5,28 @@ let questions = [ // type multichoix ou text
     },
     {
         question: "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", 
-    }
+    },
     //meryeme
 
     //mourad
+    {
+        question: "Quel est votre âge ?", type: "text", min: 15, max: 110, unite: "ans"
+    },
+    {
+        question: "Quel est votre poids ?", type: "text", min: 20, max: 220, unite: "kg"
+    },
+    {
+        question: "Quelle est votre taille ?", type: "text", min: 80, max: 250, unite: "cm"
+    },
+    {
+        question: "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ?  Ou prenez-vous un traitement à visée cardiologique ?", type: "multichoix", choix: ["oui","non","ne sait pas"]
+    },
+    {
+        question: "Êtes-vous diabétique ? ", type: "multichoix", choix: ["oui","non","ne sait pas"]
+    },
+    {
+        question: "Avez-vous ou avez-vous eu un cancer ? ", type: "multichoix", choix: ["oui","non","ne sait pas"]
+    }
 
     //said
 ]
